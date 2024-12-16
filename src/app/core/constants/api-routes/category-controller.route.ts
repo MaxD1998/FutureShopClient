9177463fba3cdb3ddf4597f1949e5a3bbce30e7b
Maks Michalski <mamich1998@gmail.com>
2024-12-16 18:05:45 +0100

    Fix

--- src/app/core/constants/api-routes/category-controller.route.ts
+++ src/app/core/constants/api-routes/category-controller.route.ts
@@ -7,7 +7,7 @@ export class CategoryControllerRoute {
   public static readonly all = `${this.productModule}All/`;
   public static readonly availableToBeChild = `${this.productModule}AvailableToBeChild/`;
   public static readonly availableToBeParent = `${this.productModule}AvailableToBeParent/`;
-  public static readonly categoryParentId = `${this.shopModule}}CategoryParentId/`;
+  public static readonly categoryParentId = `${this.shopModule}CategoryParentId/`;
   public static readonly idName = `${this.shopModule}IdName/`;
   public static readonly page = `${this.productModule}Page/`;
 }
